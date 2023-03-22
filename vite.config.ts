0ab7d0cd09eca89d458f79b1ae6af8a2085c9ee5
coderegtech <reygundanag55@gmail.com>
2023-03-22 18:36:44 +0800

    Deployed Animenia-ph Website

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  
+  base: '/animenia-ph/',
   server: {
     proxy: {
       '/api': 'https://gogoanime.consumet.stream/'
